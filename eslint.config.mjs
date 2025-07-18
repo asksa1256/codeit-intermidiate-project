d7e8cbb4c183f1c9a33b93b7c0f22ad74541a856
asksa1256@gmail.com <asksa1256@gmail.com>
2025-07-18 15:02:48 +0900

    chore: eslint import order 패키지 추가

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
-// eslint.config.mjs에 규칙 추가
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { FlatCompat } from '@eslint/eslintrc';
+import pluginImport from 'eslint-plugin-import';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,13 +11,56 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  {
+    ignores: ['.next/**'],
+  },
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
   {
+    plugins: {
+      import: pluginImport,
+    },
     rules: {
-      'no-unused-vars': 'off', // JS용 기본 비활성화
+      // 쓰이지 않는 변수 검사
+      'no-unused-vars': 'off',
       '@typescript-eslint/no-unused-vars': [
         'error',
         { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
+
+      // import order 검사
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin', // Node.js 내장 모듈
+            'external', // 외부 라이브러리
+            'internal', // 프로젝트 내부 alias
+            ['parent', 'sibling', 'index'], // 상대 경로
+            'object', // (거의 없음)
+            'type', // 타입 임포트
+          ],
+          pathGroups: [
+            {
+              pattern: 'next/**',
+              group: 'external',
+              position: 'before',
+            },
+            {
+              pattern: '@/**',
+              group: 'internal',
+            },
+            {
+              pattern: '@src/**',
+              group: 'internal',
+            },
+          ],
+          pathGroupsExcludedImportTypes: ['type'],
+          'newlines-between': 'always',
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
       ],
     },
   },
