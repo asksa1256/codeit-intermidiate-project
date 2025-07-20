681220e306f68855aac40d9b82bb405613474388
tasi <taegyung.e@gmail.com>
2025-07-20 23:55:38 +0900

    chore: DropdownItem과 코드 통일성 있게 수정 #16

--- src/components/ui/Dropdown/DropdownList.tsx
+++ src/components/ui/Dropdown/DropdownList.tsx
@@ -7,21 +7,27 @@ import { DropdownListProps } from '@/types/dropdownTypes';
 import { cn } from '@/utils/style';
 
 const defaultStyle = `absolute border border-gray-300 bg-white whitespace-nowrap outline-none rounded-[16px]`;
+
 const animationStyle = `transition duration-200 ease-out data-closed:scale-95 data-closed:opacity-0`;
+
 const alignStyle = {
   right: 'end-0 origin-top-right',
   left: 'start-0 origin-top-left',
 };
 const normalStyle = `px-[4px] py-[3px]`;
+
+const wideCommonStyle = `w-full start-0 origin-top`;
+
 const wideStyle = {
-  sm: 'w-full start-0 origin-top px-[6px] py-[5px] md:py-[6px]',
-  md: 'w-full start-0 origin-top px-[6px] py-[6px]',
+  sm: 'px-[6px] py-[5px] md:py-[6px]',
+  md: 'px-[6px] py-[6px]',
 };
 
 const DropdownList = ({ children, align = 'right', className }: DropdownListProps) => {
   const { size, wide } = useDropdownSize();
+  const checkSize = size ?? 'sm';
 
-  const isSizeStyle = wide ? wideStyle[size ?? 'sm'] : normalStyle;
+  const isSizeStyle = wide ? cn(wideCommonStyle, wideStyle[checkSize]) : normalStyle;
 
   const mergedClassName = cn(
     defaultStyle,
