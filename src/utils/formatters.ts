80375fee1ef8e4c0b939afe523b1989dfbd4665f
PC2502 <zxcv530@naver.com>
2025-07-22 00:54:48 +0900

    feat: 상대적 시간계산 유틸함수 추가(#54)

--- src/utils/formatters.ts
+++ src/utils/formatters.ts
@@ -4,3 +4,29 @@ export const formatPrice = (value: string | number) => {
   const formatValue = isNaN(price) ? '0' : price.toLocaleString('ko-KR');
   return formatValue;
 };
+
+export const formatRelativeTime = (targetDate: string) => {
+  const now = Date.now();
+  const timestampTarget = new Date(targetDate).getTime();
+
+  const passedSecond = Math.floor((now - timestampTarget) / 1000);
+  const passedMinute = Math.floor(passedSecond / 60);
+  const passedHour = Math.floor(passedMinute / 60);
+  const passedDay = Math.floor(passedHour / 24);
+  const passedMonth = Math.floor(passedDay / 30.4375);
+  const passedYear = Math.floor(passedMonth / 12);
+
+  if (passedYear > 0) {
+    return `${passedYear}년 전`;
+  } else if (passedMonth > 0) {
+    return `${passedMonth}달 전`;
+  } else if (passedDay > 0) {
+    return `${passedDay}일 전`;
+  } else if (passedHour > 0) {
+    return `${passedHour}시간 전`;
+  } else if (passedMinute > 0) {
+    return `${passedMinute}분 전`;
+  } else {
+    return `${passedSecond}초 전`;
+  }
+};
