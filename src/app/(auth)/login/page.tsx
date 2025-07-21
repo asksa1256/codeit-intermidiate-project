85bfcbe24d4958f0108b07e19a61145290276a1b
leesangdal <asksa1256@gmail.com>
2025-07-21 11:35:37 +0900

    style: 로그인 페이지 레이아웃 스타일 수정

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -2,7 +2,7 @@ import LoginForm from '@/components/feature/LoginForm';
 
 const LoginPage = () => {
   return (
-    <section className='py-20'>
+    <section className='w-full flex justify-center py-20 px-4'>
       <LoginForm />
     </section>
   );
