ef9a7efcedf040e0f462ad2732df0e1a88ca5b5e
PC2502 <zxcv530@naver.com>
2025-07-22 17:07:24 +0900

    fix:별 아이콘을 사용하도록 변경, className을 받아 컨테이너를 커스텀 하도록 변경(#62)

--- src/components/ui/RatingAndPrice.tsx
+++ src/components/ui/RatingAndPrice.tsx
@@ -1,18 +1,29 @@
+import Image from 'next/image';
+
 import { formatPrice, formatRating } from '@/utils/formatters';
+import { cn } from '@/utils/style';
 
 interface Props {
+  className?: string;
   label: 'price' | 'rating';
   value: number;
 }
 
-const RatingAndPrice = ({ label, value }: Props) => {
-  const content = label === 'price' ? `￦ ${formatPrice(value)}` : `★ ${formatRating(value)}`;
+const RatingAndPrice = ({ className, label, value }: Props) => {
+  const DEFAULT_CONTAINER_STYLE =
+    'inline-flex gap-[2px] items-center px-[10px] py-[6px] md:px-[15px] md:py-2 rounded-xl bg-primary-10 text-primary text-md md:text-lg font-bold';
+  const mergedClassName = cn(DEFAULT_CONTAINER_STYLE, className);
+  const content =
+    label === 'price' ? (
+      `￦ ${formatPrice(value)}`
+    ) : (
+      <>
+        <Image src={'/images/StarIcon.svg'} alt='별 아이콘' width={14} height={14} />
+        <span>{`${formatRating(value)}`}</span>
+      </>
+    );
 
-  return (
-    <div className='inline-flex items-center px-[10px] py-[6px] md:px-[15px] md:py-2 rounded-xl bg-primary-10 text-primary text-md md:text-lg font-bold'>
-      {content}
-    </div>
-  );
+  return <div className={mergedClassName}>{content}</div>;
 };
 
 export default RatingAndPrice;
