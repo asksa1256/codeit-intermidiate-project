da47a0bc60ef0772cf984fe46538915eacf156fc
alex <choijeaho86@gmail.com>
2025-07-22 19:08:25 +0900

    #70 간편로그인(소셜) API 개발

--- src/lib/api/axios.ts
+++ src/lib/api/axios.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+export type SocialProvider = 'GOOGLE' | 'NAVER' | 'KAKAO';
+
 /**
  * @class AxiosApiAuth
  * @description 인증 관련 API 요청을 처리하는 클래스입니다.
@@ -96,6 +98,35 @@ export class AxiosApiAuth {
       const response = await axios.post(
         `${this.requestUrl}/refresh-token`,
         { refreshToken },
+        { headers: { 'Content-Type': 'application/json' } }
+      );
+      return response.data;
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        // Axios 에러인 경우, 서버에서 받은 에러 응답 데이터를 반환합니다.
+        return error.response?.data;
+      }
+      // 그 외의 예상치 못한 에러는 다시 던집니다.
+      throw error;
+    }
+  }
+
+  /**
+   * @method signInBySocial
+   * @description 소셜 로그인을 처리합니다.
+   * @param {SocialProvider} provider - 소셜 로그인 제공자입니다. (GOOGLE, NAVER, KAKAO)
+   * @param {string} state - 상태 토큰입니다. Naver 의 경우에는 필수입니다. code를 얻을 때 사용하였던 state 값을 그대로 사용합니다.
+   * @param {string} redirectUri - 리다이렉트 URI입니다. Kakao 의 경우에는 필수입니다. 인가 코드를 얻을 때 사용하였던 redirect_uri 값을 그대로 사용합니다.
+   * @param {string} token - 인증 토큰입니다. Google 의 경우에는 Google Id 토큰(JWT) 입니다. Kakao 의 경우에는 인가 코드 입니다. Naver 의 경우에는 code 입니다.
+   * @returns {Promise<any>} - 로그인 요청의 응답 데이터를 반환합니다.
+   *                            성공 시 백엔드에서 반환하는 데이터, 실패 시 에러 응답 데이터를 포함합니다.
+   * @throws {Error} - Axios 에러가 아닌 다른 종류의 에러 발생 시 해당 에러를 던집니다.
+   */
+  async signInBySocial(provider: SocialProvider, state: string, redirectUri: string, token: string) {
+    try {
+      const response = await axios.post(
+        `${this.requestUrl}/signIn/${provider}`,
+        { state, redirectUri, token },
         { headers: { 'Content-Type': 'application/json' } }
       );
       return response.data;
