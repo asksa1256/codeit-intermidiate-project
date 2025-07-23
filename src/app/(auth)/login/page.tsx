6dbe206ef6fd0beb9171e4c2bb4f305b696fa727
asksa1256@gmail.com <asksa1256@gmail.com>
2025-07-23 17:38:12 +0900

    chore: 필요없는 주석, 컴포넌트 삭제

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,11 +1,8 @@
 import LoginForm from '@/components/feature/LoginForm';
 
-// import ClientAutoLogin from './ClientAutoLogin';
-
 const LoginPage = async () => {
   return (
     <section className='w-full flex justify-center py-20 px-4'>
-      {/* <ClientAutoLogin /> */}
       <LoginForm />
     </section>
   );
