033f21c2b621a88f976e90b0db5327d54c2ed892
alex <choijeaho86@gmail.com>
2025-07-25 22:14:20 +0900

    cicd 재배포 .env.local로 변경

--- src/app/(global)/page.tsx
+++ src/app/(global)/page.tsx
@@ -1,10 +1,10 @@
 const Home = () => {
   return (
     <main>
-      <div className="xs:bg-purple-400 sm:bg-amber-500 lg:bg-blue-400 xl:bg-pink-300">
+      <div className='xs:bg-purple-400 sm:bg-amber-500 lg:bg-blue-400 xl:bg-pink-300'>
         반응형 테스트
       </div>
-      <div className="w-[100px] h-[100px] bg-black"></div>
+      <div className='w-[100px] h-[100px] bg-black'></div>
     </main>
   );
 };
