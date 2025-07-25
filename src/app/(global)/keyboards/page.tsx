de1d39ae8c84221997ef944882961481934f03ca
kimin0401 <inptp1996@gmail.com>
2025-07-25 18:51:25 +0900

    feat: 무한 스크롤 커스텀 훅,axios 기반  api 페처 제작

--- src/app/(global)/keyboards/page.tsx
+++ src/app/(global)/keyboards/page.tsx
@@ -1,28 +1,33 @@
 // 키보드 목록 페이지
 'use client';
 
-import axios from 'axios';
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 
+import InfiniteScroll from '@/components/feature/InfiniteScroll';
 import IndexKeyboardsCard from '@/components/feature/Keyboards/IndexKeyboardsCard';
+import useInfiniteScroll from '@/hooks/useInfiniteScroll'; //  커스텀 훅
+import { fetchKeyboardsAPI } from '@/utils/api/fetchKeyboardApi'; //  API fetcher
 
-import type { KeyboardItemType, KeyboardListType } from '@/types/keyboardTypes';
+import type { KeyboardItemType } from '@/types/keyboardTypes';
 
 const KeyboardsPage = () => {
-  const [keyboards, setKeyboards] = useState<KeyboardItemType[]>([]);
+  const { items, loading, hasMore, fetchNext } =
+    useInfiniteScroll<KeyboardItemType>(fetchKeyboardsAPI);
 
   useEffect(() => {
-    axios
-      .get<KeyboardListType>('https://winereview-api.vercel.app/16-3/wines?limit=10')
-      .then((res) => setKeyboards(res.data.list))
-      .catch((err) => console.error('호출 실패', err));
-  }, []);
+    fetchNext(); // 첫 페이지 로딩
+  }, [fetchNext]);
 
   return (
-    <div>
+    <div className='p-4'>
       <h1 className='text-2xl font-bold mb-4'>키보드 페이지</h1>
-      <div className='flex flex-col gap-4'>
-        {keyboards.map((keyboard) => (
+
+      <InfiniteScroll<KeyboardItemType>
+        items={items}
+        loading={loading}
+        hasMore={hasMore}
+        fetchNext={fetchNext}
+        renderItem={(keyboard) => (
           <IndexKeyboardsCard
             key={keyboard.id}
             name={keyboard.name}
@@ -33,8 +38,8 @@ const KeyboardsPage = () => {
             reviewCount={keyboard.reviewCount}
             recentReview={keyboard.recentReview || null}
           />
-        ))}
-      </div>
+        )}
+      />
     </div>
   );
 };
