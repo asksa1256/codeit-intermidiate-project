8e4a8c22886826e2ffec0b5068c9b1cb687c5c05
tasi <taegyung.e@gmail.com>
2025-07-26 19:48:54 +0900

    feat: myprofile 후기 리스트 API 연동 및 리뷰 없을때 UI 렌더링 #130

--- src/components/feature/myProfile/MyReviewList.tsx
+++ src/components/feature/myProfile/MyReviewList.tsx
@@ -1,160 +1,63 @@
-import MyReviewItem from '@/components/feature/myProfile/MyReviewItem';
-import { MyReviewListType } from '@/types/reviewTypes';
+'use client';
 
-const reviewsData: MyReviewListType = {
-  list: [
-    {
-      id: 3054,
-      rating: 2,
-      lightBold: 6,
-      smoothTannic: 8,
-      drySweet: 4,
-      softAcidic: 5,
-      aroma: ['CHERRY'],
-      content: '저한테는 너무 맞지 않네요...',
-      createdAt: '2025-07-21T06:02:06.037Z',
-      updatedAt: '2025-07-21T06:02:06.037Z',
-      user: {
-        id: 1642,
-        nickname: '테스트163',
-        image: null,
-      },
-      wine: {
-        id: 1345,
-        name: '키크론 V10 MAX 유무선 앨리스 키보드',
-        region: '키크론',
-        image:
-          '<https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/Wine/user/1642/1753074281491/%C3%AD%C2%82%C2%A4%C3%AB%C2%B3%C2%B4%C3%AB%C2%93%C2%9C0_%C3%AD%C2%82%C2%A4%C3%AD%C2%81%C2%AC%C3%AB%C2%A1%C2%A0%C3%AC%C2%95%C2%A8%C3%AB%C2%A6%C2%AC%C3%AC%C2%8A%C2%A4.png>',
-        price: 169000,
-        avgRating: 3.833333333333333,
-        type: 'RED',
-      },
-    },
-    {
-      id: 3055,
-      rating: 5,
-      lightBold: 6,
-      smoothTannic: 8,
-      drySweet: 4,
-      softAcidic: 5,
-      aroma: ['CHERRY'],
-      content: '적응만 되면 최고의 키보드!!! 키감이 쫀득하고 소리가 너무 좋아요!!!!',
-      createdAt: '2025-07-21T06:02:23.888Z',
-      updatedAt: '2025-07-21T06:02:23.888Z',
-      user: {
-        id: 1642,
-        nickname: '테스트163',
-        image: null,
-      },
-      wine: {
-        id: 1345,
-        name: '키크론 V10 MAX 유무선 앨리스 키보드',
-        region: '키크론',
-        image:
-          '<https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/Wine/user/1642/1753074281491/%C3%AD%C2%82%C2%A4%C3%AB%C2%B3%C2%B4%C3%AB%C2%93%C2%9C0_%C3%AD%C2%82%C2%A4%C3%AD%C2%81%C2%AC%C3%AB%C2%A1%C2%A0%C3%AC%C2%95%C2%A8%C3%AB%C2%A6%C2%AC%C3%AC%C2%8A%C2%A4.png>',
-        price: 169000,
-        avgRating: 3.833333333333333,
-        type: 'RED',
-      },
-    },
-    {
-      id: 3056,
-      rating: 5,
-      lightBold: 6,
-      smoothTannic: 8,
-      drySweet: 4,
-      softAcidic: 5,
-      aroma: ['BAKING', 'TOBACCO'],
-      content: '적응만 되면 최고의 키보드!!! 키감이 쫀득하고 소리가 너무 좋아요!!!!',
-      createdAt: '2025-07-21T06:05:22.047Z',
-      updatedAt: '2025-07-21T06:05:22.047Z',
-      user: {
-        id: 1642,
-        nickname: '테스트163',
-        image: null,
-      },
-      wine: {
-        id: 1345,
-        name: '키크론 V10 MAX 유무선 앨리스 키보드',
-        region: '키크론',
-        image:
-          '<https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/Wine/user/1642/1753074281491/%C3%AD%C2%82%C2%A4%C3%AB%C2%B3%C2%B4%C3%AB%C2%93%C2%9C0_%C3%AD%C2%82%C2%A4%C3%AD%C2%81%C2%AC%C3%AB%C2%A1%C2%A0%C3%AC%C2%95%C2%A8%C3%AB%C2%A6%C2%AC%C3%AC%C2%8A%C2%A4.png>',
-        price: 169000,
-        avgRating: 3.833333333333333,
-        type: 'RED',
-      },
-    },
-    {
-      id: 3057,
-      rating: 4,
-      lightBold: 5,
-      smoothTannic: 6,
-      drySweet: 7,
-      softAcidic: 8,
-      aroma: ['CHERRY'],
-      content: '손목이 너무 아파서 구매했습니다. 잘 맞았으면 좋겠네요. 키감은 만족합니다.',
-      createdAt: '2025-07-21T06:17:12.701Z',
-      updatedAt: '2025-07-21T06:17:12.701Z',
-      user: {
-        id: 1642,
-        nickname: '테스트163',
-        image: null,
-      },
-      wine: {
-        id: 1345,
-        name: '키크론 V10 MAX 유무선 앨리스 키보드',
-        region: '키크론',
-        image:
-          '<https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/Wine/user/1642/1753074281491/%C3%AD%C2%82%C2%A4%C3%AB%C2%B3%C2%B4%C3%AB%C2%93%C2%9C0_%C3%AD%C2%82%C2%A4%C3%AD%C2%81%C2%AC%C3%AB%C2%A1%C2%A0%C3%AC%C2%95%C2%A8%C3%AB%C2%A6%C2%AC%C3%AC%C2%8A%C2%A4.png>',
-        price: 169000,
-        avgRating: 3.833333333333333,
-        type: 'RED',
-      },
-    },
-    {
-      id: 3058,
-      rating: 4,
-      lightBold: 5,
-      smoothTannic: 6,
-      drySweet: 7,
-      softAcidic: 8,
-      aroma: ['BERRY'],
-      content:
-        '트렌드라고 하길래 구매했어용! 배열이 진짜 신기하게 생겼네용! LED도 있고 화려해서 마음에 들어용! 잘쓸께용^^!',
-      createdAt: '2025-07-21T06:18:41.851Z',
-      updatedAt: '2025-07-21T06:18:41.851Z',
-      user: {
-        id: 1642,
-        nickname: '테스트163',
-        image: null,
-      },
-      wine: {
-        id: 1345,
-        name: '키크론 V10 MAX 유무선 앨리스 키보드',
-        region: '키크론',
-        image:
-          '<https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/Wine/user/1642/1753074281491/%C3%AD%C2%82%C2%A4%C3%AB%C2%B3%C2%B4%C3%AB%C2%93%C2%9C0_%C3%AD%C2%82%C2%A4%C3%AD%C2%81%C2%AC%C3%AB%C2%A1%C2%A0%C3%AC%C2%95%C2%A8%C3%AB%C2%A6%C2%AC%C3%AC%C2%8A%C2%A4.png>',
-        price: 169000,
-        avgRating: 3.833333333333333,
-        type: 'RED',
-      },
-    },
-  ],
-  totalCount: 24,
-  nextCursor: 3058,
-};
+import Link from 'next/link';
+
+import { useEffect, useState } from 'react';
+
+import MyReviewItem from '@/components/feature/myProfile/MyReviewItem';
+import EmptyList from '@/components/ui/EmptyList';
+import { apiClient } from '@/lib/api/apiClient';
+import { MyReviewItemType, MyReviewListType } from '@/types/reviewTypes';
+
+const TEAM = process.env.NEXT_PUBLIC_TEAM;
+const DEFAULT_LIMIT = 10;
 
 const MyReviewList = () => {
+  const [reviewList, setReviewList] = useState<MyReviewItemType[] | null>(null);
+  const [nextCursor, setNextCursor] = useState<number | null>(null);
+  const [totalCount, setTotalCount] = useState<number>(0);
+
+  useEffect(() => {
+    const getReviewList = async () => {
+      try {
+        const res = await apiClient.get(`/${TEAM}/users/me/reviews?limit=${DEFAULT_LIMIT}`);
+        const data: MyReviewListType = res.data;
+        const { list, nextCursor, totalCount } = data;
+
+        setReviewList(list);
+        setTotalCount(totalCount);
+        setNextCursor(nextCursor);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    getReviewList();
+  }, []);
+
   return (
     <>
-      <span className='absolute bottom-[calc(100%+16px)] right-0 text-xs text-primary leading-[26px] md:text-md md:leading-[32px] md:bottom-[calc(100%+22px)]'>
-        총 {reviewsData.totalCount}개
+      <span className='rounded-2xl absolute bottom-[calc(100%+16px)] right-0 text-xs text-primary leading-[26px] md:text-md md:leading-[32px] md:bottom-[calc(100%+22px)]'>
+        총 {totalCount}개
       </span>
-      <ul>
-        {reviewsData.list.map((review) => (
-          <MyReviewItem key={review.id} review={review} />
-        ))}
-      </ul>
+      <div>
+        {!reviewList ? (
+          <EmptyList desc='작성된 리뷰가 없어요'>
+            <Link
+              href='/keyboards'
+              className='inline-flex items-center justify-center px-[15px] h-[48px] font-semibold text-white bg-primary rounded-xl md:px-[24px]'
+            >
+              키보드 구경 하러 가기
+            </Link>
+          </EmptyList>
+        ) : (
+          <ul>
+            {reviewList.map((review) => (
+              <MyReviewItem key={review.id} review={review} />
+            ))}
+          </ul>
+        )}
+      </div>
     </>
   );
 };
