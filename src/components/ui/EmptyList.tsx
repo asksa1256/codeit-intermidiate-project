4f1de2c7270db182716ccffd932adc43919e55d7
tasi <taegyung.e@gmail.com>
2025-07-26 19:11:29 +0900

    feat: 데이터 없을 때 렌더링 UI 컴포넌트 제작

---
+++ src/components/ui/EmptyList.tsx
@@ -0,0 +1,32 @@
+import Image from 'next/image';
+
+import { ReactNode } from 'react';
+
+import { cn } from '@/utils/style';
+
+interface Props {
+  desc: string;
+  children?: ReactNode;
+  className?: string;
+}
+
+const EmptyList = ({ desc, children, className }: Props) => {
+  const defaultStyle = 'flex items-center justify-center flex-col min-h-[375px] md:min-h-[442px]';
+  const mergedClassName = cn(defaultStyle, className);
+
+  return (
+    <div className={mergedClassName}>
+      <Image
+        src='/images/EmptyListIcon.svg'
+        alt='데이터가 없습니다'
+        width={136}
+        height={136}
+        className='w-[100px] h-auto md:w-[136px]'
+      />
+      <p className='mt-8 mb-10 text-gray-500 text-center md:mt-6 md:mb-12 md:text-lg'>{desc}</p>
+      {children}
+    </div>
+  );
+};
+
+export default EmptyList;
