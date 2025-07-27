6c927874095e4baa0a5c4698f4a9adab150d0eb7
tasi <taegyung.e@gmail.com>
2025-07-27 17:56:13 +0900

    chore: 삭제하기 컨펌 모달 추가 및 수정하기 모달 위치 변경 #130

--- src/components/feature/myProfile/MyReviewList.tsx
+++ src/components/feature/myProfile/MyReviewList.tsx
@@ -1,8 +1,3 @@
-'use client';
-
-import { useState } from 'react';
-
-import Modal from '@/components/feature/Modal';
 import MyReviewItem from '@/components/feature/myProfile/MyReviewItem';
 import { MyReviewItemType } from '@/types/reviewTypes';
 
@@ -12,36 +7,13 @@ interface Props {
 }
 
 const MyReviewList = ({ reviewList, onReviewDelete }: Props) => {
-  const [modalOpen, setModalOpen] = useState(false);
-  const [currentReview, setCurrentReview] = useState<MyReviewItemType | null>(null);
-
-  // 리뷰 수정 모달 열기
-  const handleOpenModal = (review: MyReviewItemType) => {
-    setModalOpen(true);
-    setCurrentReview(review);
-  };
-
-  // 리뷰 수정 모달 닫기
-  const handleCloseModal = () => {
-    setModalOpen(false);
-    setCurrentReview(null);
-  };
-
   return (
     <>
       <ul>
         {reviewList.map((review) => (
-          <MyReviewItem
-            key={review.id}
-            review={review}
-            onDelete={onReviewDelete}
-            onEdit={handleOpenModal}
-          />
+          <MyReviewItem key={review.id} review={review} onDelete={onReviewDelete} />
         ))}
       </ul>
-      <Modal open={modalOpen} onClose={handleCloseModal} title='수정하기'>
-        여기 폼이 들어감
-      </Modal>
     </>
   );
 };
