318878cd7723f478c5f1d1ef6f2ace606f3a58f6
tasi <taegyung.e@gmail.com>
2025-07-27 19:43:25 +0900

    chore: import 순서 수정

--- src/components/feature/myProfile/MyprofileMain.tsx
+++ src/components/feature/myProfile/MyprofileMain.tsx
@@ -1,7 +1,7 @@
 import { Tab, TabGroup, TabList, TabPanel, TabPanels } from '@headlessui/react';
 
-import MyReviewArea from '@/components/feature/myProfile/MyReviewArea';
 import MyKeyboardArea from '@/components/feature/myProfile/MyKeyboardArea';
+import MyReviewArea from '@/components/feature/myProfile/MyReviewArea';
 
 const MYPAGE_MENUS = [
   { title: '내가 쓴 후기', component: MyReviewArea },
