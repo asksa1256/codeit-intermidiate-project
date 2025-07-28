5c7967f41b90ca8867977b92cc767da010a9190a
tasi <taegyung.e@gmail.com>
2025-07-28 17:06:04 +0900

    chore: KEYBOARD_COLOR_MAP 상수 constants 파일로 이동 #132

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -16,3 +16,26 @@ export const KAKAO_LOGIN_PAGE = '/oauth/kakao';
 // images
 export const DEFAULT_PROFILE_IMG_URL =
   'https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/Wine/user/1693/1753524666758/UserThumbEmpty.png'; // api 요청 에러 방지용 기본 이미지 url
+
+// aroma 속성 색상값 매핑
+export const KEYBOARD_COLOR_MAP = {
+  CHERRY: '레드',
+  BERRY: '퍼플',
+  OAK: '브라운',
+  VANILLA: '베이지',
+  PEPPER: '블랙',
+  BAKING: '연노랑',
+  GRASS: '그린',
+  APPLE: '라임',
+  PEACH: '핑크',
+  CITRUS: '오렌지',
+  TROPICAL: '레인보우',
+  MINERAL: '투명',
+  FLOWER: '연보라',
+  TOBACCO: '그레이',
+  EARTH: '블루',
+  CHOCOLATE: '스카이',
+  SPICE: '옐로우',
+  CARAMEL: '반투명',
+  LEATHER: '화이트',
+};
