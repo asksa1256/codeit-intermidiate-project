2fe26f2458959e6fe3723bcbb25fa181de90fb46
kimin0401 <inptp1996@gmail.com>
2025-07-28 14:41:21 +0900

    fix: EmptyList 컴포넌트 사용

--- src/app/(global)/keyboards/page.tsx
+++ src/app/(global)/keyboards/page.tsx
@@ -6,6 +6,7 @@ import { useEffect, useState } from 'react';
 
 import IndexKeyboardsCard from '@/components/feature/Keyboards/IndexKeyboardsCard';
 import KeyboardsSearchBar from '@/components/feature/Keyboards/KeyboardsSearchBar';
+import EmptyList from '@/components/ui/EmptyList';
 
 import type { KeyboardItemType } from '@/types/keyboardTypes';
 
@@ -17,6 +18,10 @@ interface FetchParams {
 const KeyboardsPage = () => {
   const [items, setItems] = useState<KeyboardItemType[]>([]);
   const [searchResults, setSearchResults] = useState<KeyboardItemType[] | null>(null);
+  const dataToRender = searchResults && searchResults.length > 0 ? searchResults : items;
+
+  const isSearching = searchResults !== null;
+  const isSearchEmpty = isSearching && searchResults?.length === 0;
 
   // 커서를 돌리며 키보드 리스트 불러오기
   useEffect(() => {
@@ -48,29 +53,29 @@ const KeyboardsPage = () => {
     fetchAllItems();
   }, []);
 
-  // 검색 결과가 있으면 그걸 렌더링, 없으면 기본 아이템 렌더링
-  const dataToRender = searchResults && searchResults.length > 0 ? searchResults : items;
-
   return (
     <div className='p-4'>
       <h1 className='text-2xl font-bold mb-4'>키보드 페이지</h1>
       {/* 검색창: 검색 결과를 setSearchResults로 전달 */}
       <KeyboardsSearchBar onSearchResults={setSearchResults} />
-
-      <div className='mt-4 grid grid-cols-1 gap-4 md:gap-6 lg:gap-8'>
-        {dataToRender.map((item) => (
-          <IndexKeyboardsCard
-            key={item.id}
-            name={item.name}
-            region={item.region}
-            image={item.image}
-            price={item.price}
-            avgRating={item.avgRating}
-            reviewCount={item.reviewCount}
-            recentReview={item.recentReview}
-          />
-        ))}
-      </div>
+      {isSearchEmpty ? (
+        <EmptyList desc='검색 결과가 없습니다.' />
+      ) : (
+        <div className='mt-4 grid grid-cols-1 gap-4 md:gap-6 lg:gap-8'>
+          {dataToRender.map((item) => (
+            <IndexKeyboardsCard
+              key={item.id}
+              name={item.name}
+              region={item.region}
+              image={item.image}
+              price={item.price}
+              avgRating={item.avgRating}
+              reviewCount={item.reviewCount}
+              recentReview={item.recentReview}
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
