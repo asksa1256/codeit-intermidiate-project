8f84dec544bc7c9bc22b3c272f772bd77a5d2469
leesangdal <asksa1256@gmail.com>
2025-07-28 18:28:12 +0900

    style: 키보드 이미지 등록 스타일 수정 (그림자, 가운데 정렬 추가)

--- src/components/feature/ImageUpload/ImageUploader.tsx
+++ src/components/feature/ImageUpload/ImageUploader.tsx
@@ -35,13 +35,13 @@ const ImageUploader = ({ value, error, onChange }: ImageUploaderProps) => {
       <input type='file' className='hidden' ref={fileRef} onChange={handleImageUrl} />
 
       {value && (
-        <figure className='relative w-[140px] aspect-square rounded-2xl overflow-hidden ml-2 border-2 border-primary bg-primary-10'>
+        <figure className='relative flex justify-center items-center w-[140px] aspect-square rounded-2xl overflow-hidden ml-2 border-2 border-primary bg-primary-10'>
           <Image
             src={value}
             alt='이미지'
             width={140}
             height={140}
-            className='object-contain rotate-90 h-full w-4/5 drop-shadow-[4_4px_6px_rgba(0,0,0,0.15)]'
+            className='object-contain rotate-90 h-full w-4/5 drop-shadow-[4px_4px_6px_rgba(0,0,0,0.15)]'
           />
         </figure>
       )}
