abf0c482e2b68aeb8c71ca9073218e00015a68b0
asksa1256@gmail.com <asksa1256@gmail.com>
2025-07-29 15:12:58 +0900

    refactor: 로딩스피너 className cn함수 적용

--- src/components/ui/LoadingSpinner.tsx
+++ src/components/ui/LoadingSpinner.tsx
@@ -1,3 +1,5 @@
+import { cn } from '@/utils/style';
+
 interface LoadingSpinnerProps {
   text?: string;
   className?: string;
@@ -5,7 +7,7 @@ interface LoadingSpinnerProps {
 
 const LoadingSpinner = ({ text, className }: LoadingSpinnerProps) => {
   return (
-    <section className={`flex flex-col items-center justify-center ${className}`}>
+    <section className={cn('flex flex-col items-center justify-center', className)}>
       <div className='w-8 h-8 border-4 border-gray-300 border-t-primary rounded-full animate-spin' />
       {text && <p className='mt-4'>{text}</p>}
     </section>
