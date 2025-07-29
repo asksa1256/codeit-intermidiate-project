176d1a34b010bd76c07caf8f91340618a2122798
PC2502 <zxcv530@naver.com>
2025-07-29 15:46:38 +0900

    feat: 상세 페이지 로딩스피너 추가(#60)

--- src/app/(global)/keyboards/[keyboardid]/page.tsx
+++ src/app/(global)/keyboards/[keyboardid]/page.tsx
@@ -7,21 +7,26 @@ import { useEffect, useState } from 'react';
 import KeyboardInfoCard from '@/components/feature/keyboardDetails/KeyboardInfoCard';
 import RatingsInfo from '@/components/feature/keyboardDetails/RatingsInfo';
 import ReviewList from '@/components/feature/keyboardDetails/ReviewList';
+import LoadingSpinner from '@/components/ui/LoadingSpinner';
 import { apiClient } from '@/lib/api/apiClient';
 import { KeyboardDetailType } from '@/types/keyboardTypes';
 
 const KeyboardDetailsPage = () => {
   const [keyboardInfo, setKeyboardInfo] = useState<KeyboardDetailType | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
   const params = useParams();
   const { keyboardid } = params;
 
   const getKeyboardInfo = async () => {
     try {
+      setIsLoading(true);
       const res = await apiClient(`/${process.env.NEXT_PUBLIC_TEAM}/wines/${keyboardid}`);
       setKeyboardInfo(res.data);
     } catch (e) {
       console.log('키보드 상세 데이터 받아오기 실패');
       throw e;
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -35,11 +40,17 @@ const KeyboardDetailsPage = () => {
 
   return (
     <main className='lg:max-w-285 px-4 pt-[30px] pb-10 md:px-5 lg:px-0 md:pt-10 md:pb-20 lg:pt-10 lg:mx-auto'>
-      <KeyboardInfoCard keyboardInfo={keyboardInfo} />
-      <div className='lg:flex lg:items-start lg:gap-15 lg:justify-between'>
-        <RatingsInfo keyboardInfo={keyboardInfo} />
-        <ReviewList reviewList={keyboardInfo['reviews']} />
-      </div>
+      {isLoading ? (
+        <LoadingSpinner />
+      ) : (
+        <>
+          <KeyboardInfoCard keyboardInfo={keyboardInfo} />
+          <div className='lg:flex lg:items-start lg:gap-15 lg:justify-between'>
+            <RatingsInfo keyboardInfo={keyboardInfo} />
+            <ReviewList reviewList={keyboardInfo['reviews']} />
+          </div>
+        </>
+      )}
     </main>
   );
 };
