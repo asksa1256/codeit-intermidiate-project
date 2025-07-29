0f9cf04c86d05414284843d89ab80503cbcb8703
tasi <taegyung.e@gmail.com>
2025-07-30 00:49:29 +0900

    feat: ReviewForm 퍼블리싱 및 기능 구현 #132

--- src/components/feature/reviewForm/ReviewForm.tsx
+++ src/components/feature/reviewForm/ReviewForm.tsx
@@ -2,25 +2,59 @@
 
 import Image from 'next/image';
 
-import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
-import ReviewAttributeSlider from '@/components/feature/reviewForm/ReviewAttributeSlider';
-import ReviewColorCheckbox from '@/components/feature/reviewForm/ReviewColorCheckbox';
+import ReviewFormCheckbox from '@/components/feature/reviewForm/ReviewFormCheckbox';
+import ReviewFormRating from '@/components/feature/reviewForm/ReviewFormRating';
+import ReviewFormSlider from '@/components/feature/reviewForm/ReviewFormSlider';
 import ButtonDefault from '@/components/ui/ButtonDefault';
-import StarRatingInput from '@/components/ui/StarRatingInput';
-import { KEYBOARD_COLOR_MAP } from '@/constants';
 import { KeyboardColorType } from '@/types/keyboardTypes';
 import { MyReviewItemType } from '@/types/reviewTypes';
 
-interface Props {
-  review?: MyReviewItemType | null;
+export interface ReviewFormValues {
+  rating: number;
+  lightBold: number;
+  smoothTannic: number;
+  drySweet: number;
+  softAcidic: number;
+  aroma: KeyboardColorType[];
+  content: string;
 }
 
-const ReviewForm = ({ review }: Props) => {
-  const [lightBold, setLightBold] = useState<number>(0);
+interface Props {
+  keyboardTitle: string;
+  initReview?: MyReviewItemType | null;
+  onSubmit: (value: ReviewFormValues) => void;
+}
+
+const ReviewForm = ({ keyboardTitle, initReview = null, onSubmit }: Props) => {
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { isSubmitting, isValid, isDirty },
+  } = useForm<ReviewFormValues>({
+    mode: 'onChange',
+    defaultValues: {
+      rating: initReview?.rating ?? 0,
+      lightBold: initReview?.lightBold ?? 5,
+      smoothTannic: initReview?.smoothTannic ?? 5,
+      drySweet: initReview?.drySweet ?? 5,
+      softAcidic: initReview?.softAcidic ?? 5,
+      content: initReview?.content ?? '',
+      aroma: initReview?.aroma ?? [],
+    },
+  });
+
+  const handleSubmitForm = (formValues: ReviewFormValues) => {
+    onSubmit(formValues);
+  };
+
+  const isEdit = initReview !== null;
+  const isSubmitDisabled = isEdit ? !(isDirty && isValid) : !isValid;
 
   return (
-    <form className='mt-10 -mb-4'>
+    <form className='mt-10 -mb-4' onSubmit={handleSubmit(handleSubmitForm)}>
       <div className='mb-10'>
         <div className='flex items-center gap-4'>
           <figure className='flex items-center justify-center w-[65px] h-[65px] bg-gray-100 shrink-0 rounded-lg md:w-[68px] md:h-[68px]'>
@@ -32,62 +66,62 @@ const ReviewForm = ({ review }: Props) => {
             />
           </figure>
           <div className='grow'>
-            <h3 className='font-bold break-keep md:text-lg md:font-semibold'>
-              Sentinel Carbernet Sauvignon 2016
-            </h3>
-            <StarRatingInput updater={(value) => console.log(value)} className='mt-2 flex' />
+            <h3 className='font-bold break-keep md:text-lg md:font-semibold'>{keyboardTitle}</h3>
+            <ReviewFormRating name='rating' control={control} />
           </div>
         </div>
         <textarea
-          name='content'
-          id='content'
           placeholder='후기를 작성해 주세요'
           className='block w-full h-[100px] mt-6 py-[14px] px-[20px] border border-gray-300 text-md resize-none rounded-xl outline-none placeholder:text-gray-500 md:h-[120px] md:rounded-2xl md:text-base'
+          {...register('content', { required: true })}
         />
       </div>
 
       <div className='mb-10'>
         <h4 className='mb-6 text-lg font-bold'>키보드는 어땠나요?</h4>
         <div className='flex flex-col gap-4 md:gap-[18px]'>
-          <ReviewAttributeSlider
+          <ReviewFormSlider
             title='무게감'
-            onUpdate={setLightBold}
             minLabel='가벼움'
             maxLabel='무거움'
+            name='lightBold'
+            control={control}
           />
-          <ReviewAttributeSlider
+          <ReviewFormSlider
             title='타건감'
-            onUpdate={setLightBold}
             minLabel='부드러움'
             maxLabel='선명함'
+            name='smoothTannic'
+            control={control}
           />
-          <ReviewAttributeSlider
+          <ReviewFormSlider
             title='소리'
-            onUpdate={setLightBold}
             minLabel='조용함'
             maxLabel='경쾌함'
+            name='drySweet'
+            control={control}
           />
-          <ReviewAttributeSlider
+          <ReviewFormSlider
             title='반발력'
-            onUpdate={setLightBold}
             minLabel='약함'
             maxLabel='강함'
+            name='softAcidic'
+            control={control}
           />
         </div>
       </div>
 
       <div className='mb-[53px] md:mb-12'>
         <h4 className='mb-6 text-lg font-bold'>키보드에 포함된 색상들을 선택해주세요.</h4>
-
         <div className='flex flex-wrap gap-2 md:gap-[10px]'>
-          {(Object.entries(KEYBOARD_COLOR_MAP) as [KeyboardColorType, string][]).map(
-            ([color, label]) => (
-              <ReviewColorCheckbox key={color} color={color} label={label} />
-            ),
-          )}
+          <ReviewFormCheckbox name='aroma' control={control} />
         </div>
       </div>
-      <ButtonDefault disabled type='submit' className='w-full h-[54px] rounded-xl'>
+      <ButtonDefault
+        disabled={isSubmitDisabled || isSubmitting}
+        type='submit'
+        className='w-full h-[54px] rounded-xl'
+      >
         리뷰 남기기
       </ButtonDefault>
     </form>
