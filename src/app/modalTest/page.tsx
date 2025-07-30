e6f068119542df7ebee7d56d1f2a3abd4dbe5053
leesangdal <asksa1256@gmail.com>
2025-07-30 14:06:36 +0900

    chore: 모달테스트 페이지 - 키보드 등록/수정 폼 테스트 코드 추가

--- src/app/modalTest/page.tsx
+++ src/app/modalTest/page.tsx
@@ -1,14 +1,65 @@
 'use client';
 
-import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+
+import { useEffect, useState } from 'react';
 
 import KeyboardForm from '@/components/feature/Form/KeyboardForm';
+import { KeyboardFormValues } from '@/components/feature/Form/KeyboardForm';
 import Modal from '@/components/feature/Modal';
+import { TEAM_ID } from '@/constants';
+import { apiClient } from '@/lib/api/apiClient';
 
 export default function KeyboardsPage() {
   // 모달 열림/닫힘 상태
   const [open, setOpen] = useState(false);
   const [keyboardOpen, setKeyboardOpen] = useState(false);
+  const [tempInitialValues, setTempInitialValues] = useState<KeyboardFormValues>();
+
+  const router = useRouter();
+
+  const handleSubmit = async (formData: KeyboardFormValues) => {
+    const payload = { ...formData, price: +formData.price };
+    const updatePayload = { ...payload, avgRating: 4 };
+
+    // 키보드 등록 api
+    // try {
+    //   const res = await apiClient.post(`/${TEAM_ID}/wines`, payload);
+    //   const data = res?.data;
+
+    //   router.replace(`/keyboards/${data.id}`);
+    // } catch (err) {
+    //   throw err; // 폼에 에러 전달
+    // }
+
+    // 키보드 수정 api
+    try {
+      const res = await apiClient.patch(`/${TEAM_ID}/wines/1398`, updatePayload);
+      const data = res?.data;
+
+      router.replace(`/keyboards/${data.id}`);
+    } catch (err) {
+      throw err; // 폼에 에러 전달
+    }
+  };
+
+  useEffect(() => {
+    const getValues = async () => {
+      const res = await apiClient.get(`/${TEAM_ID}/wines/1398`);
+      const data = res.data;
+
+      const sanitizedData: KeyboardFormValues = {
+        name: data.name,
+        price: data.price,
+        region: data.region,
+        type: data.type,
+        image: data.image,
+      };
+
+      setTempInitialValues(sanitizedData);
+    };
+    getValues();
+  }, []);
 
   return (
     <section className='p-8'>
@@ -56,7 +107,11 @@ export default function KeyboardsPage() {
           size='md'
           showCloseButton={true}
         >
-          <KeyboardForm onClose={() => setKeyboardOpen(false)} />
+          <KeyboardForm
+            initialValues={tempInitialValues}
+            onSubmit={handleSubmit}
+            onClose={() => setKeyboardOpen(false)}
+          />
         </Modal>
       </section>
     </section>
