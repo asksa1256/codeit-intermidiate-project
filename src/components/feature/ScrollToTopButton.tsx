68c520973f2bcf23af2377af872efc0f0fbe9b42
leesangdal <asksa1256@gmail.com>
2025-07-31 00:04:56 +0900

    feat: 맨위로 가기 버튼 추가, IconButton 타입 수정

---
+++ src/components/feature/ScrollToTopButton.tsx
@@ -0,0 +1,59 @@
+'use client';
+
+import clsx from 'clsx';
+import { useState, useEffect } from 'react';
+
+import RightArrowIcon from '@/assets/icons/RightArrowIcon.svg';
+import IconButton from '@/components/ui/Button/IconButton';
+
+const ScrollToTopButton = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleScrollToTop = () => {
+    // smooth scroll
+    const start = window.scrollY;
+    const startTime = performance.now();
+    const duration = 600; // 전체 애니메이션 시간(ms)
+
+    const easeInOutCubic = (t: number) =>
+      t < 0.5 ? 4 * t * t * t : 1 - Math.pow(-2 * t + 2, 3) / 2;
+
+    const animateScroll = (currentTime: number) => {
+      const elapsed = currentTime - startTime;
+      const progress = Math.min(elapsed / duration, 1); // 0 ~ 1
+      const ease = easeInOutCubic(progress);
+
+      window.scrollTo(0, start * (1 - ease));
+
+      if (progress < 1) {
+        requestAnimationFrame(animateScroll);
+      }
+    };
+
+    requestAnimationFrame(animateScroll);
+  };
+
+  useEffect(() => {
+    const toggleVisibility = () => {
+      setIsVisible(window.scrollY > 300); // 300px 이상 스크롤 시 버튼 표시
+    };
+
+    window.addEventListener('scroll', toggleVisibility);
+    return () => window.removeEventListener('scroll', toggleVisibility);
+  }, []);
+
+  return (
+    <IconButton
+      icon={RightArrowIcon}
+      rounded
+      className={clsx(
+        'fixed bottom-10 right-10 z-[9] -rotate-90 transition-all duration-300 ease-in-out',
+        isVisible ? 'translate-y-0 pointer-events-auto' : 'translate-y-5 pointer-events-none',
+      )}
+      style={{ opacity: isVisible ? '100' : '0' }}
+      onClick={handleScrollToTop}
+    />
+  );
+};
+
+export default ScrollToTopButton;
--- src/components/ui/Button/IconButton.tsx
+++ src/components/ui/Button/IconButton.tsx
@@ -1,35 +1,35 @@
 import { Button } from '@headlessui/react';
 
 import { cn } from '@/utils/style';
-
-interface IconButtonProps {
+interface IconButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
   type?: 'submit' | 'button';
   rounded?: boolean;
   icon: React.FC<React.SVGProps<SVGSVGElement>>;
   iconReverse?: boolean;
-  className?: string;
   disabled?: boolean;
-  onClick: () => void;
 }
 
 const IconButton = ({
   type = 'button',
   rounded,
   icon: Icon,
-  className,
   iconReverse,
   disabled = false,
   onClick,
+  ...props
 }: IconButtonProps) => {
   const IconButtonBase = `group flex items-center justify-center w-[48px] h-[48px] bg-white border border-gray-300 hover:bg-primary-10 hover:border-primary transition-all ${rounded ? 'rounded-full' : 'rounded-md'} ${disabled && 'opacity-0 invisible'}`;
   return (
     <Button
       type={type}
       onClick={onClick}
-      className={cn(IconButtonBase, className)}
+      className={cn(IconButtonBase, props.className)}
       disabled={disabled}
+      style={props.style}
     >
-      <Icon className={`text-gray-500 group-hover:text-primary ${iconReverse && 'rotate-y-180'}`} />
+      <Icon
+        className={`text-gray-500 group-hover:text-primary ${iconReverse ? 'rotate-y-180' : ''}`}
+      />
     </Button>
   );
 };
