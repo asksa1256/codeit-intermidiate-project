0e931b053eefc9fe4374f57cb09b1d7e6341490e
tasi <taegyung.e@gmail.com>
2025-07-30 23:17:06 +0900

    chore: 모달창 열림시 overflow hidden 적용 및 모달 영역외 클릭시 닫힘 적용 #176

--- src/components/feature/Keyboards/Filter/FilterModalBack.tsx
+++ src/components/feature/Keyboards/Filter/FilterModalBack.tsx
@@ -5,6 +5,7 @@ import { useEffect } from 'react';
 import CloseIcon from '@/assets/icons/CloseIcon.svg';
 import FilterForm from '@/components/feature/Form/FilterForm';
 import ButtonDefault from '@/components/ui/ButtonDefault';
+import useOutsideClick from '@/hooks/useClickOutside';
 import useWindowWidth from '@/hooks/useWindowWidth';
 
 interface Props {
@@ -17,6 +18,16 @@ const FilterModalBack = ({ open, onClose }: Props) => {
   const isWeb = innerWidth > 1280;
   const isModalOpenOrWeb = open || isWeb;
   const handleClose = () => onClose(false);
+  const ref = useOutsideClick(handleClose);
+
+  useEffect(() => {
+    const htmlElement = document.documentElement;
+    if (open) {
+      htmlElement.classList.add('modal-open');
+    } else {
+      htmlElement.classList.remove('modal-open');
+    }
+  }, [open]);
 
   useEffect(() => {
     if (open && isWeb) {
@@ -28,7 +39,10 @@ const FilterModalBack = ({ open, onClose }: Props) => {
     <>
       {isModalOpenOrWeb && (
         <div className='fixed top-0 left-0 h-dvh w-full z-50 bg-black/30 flex items-end justify-center md:items-center lg:static lg:block lg:w-[300px] lg:h-auto lg:shrink-0 lg:bg-transparent lg:z-auto lg:pb-4'>
-          <div className='flex flex-col w-full max-h-[85dvh] bg-white rounded-t-2xl md:max-w-[375px] md:rounded-2xl lg:rounded-none lg:max-h-none'>
+          <div
+            className='flex flex-col w-full max-h-[85dvh] bg-white rounded-t-2xl md:max-w-[375px] md:rounded-2xl lg:rounded-none lg:max-h-none'
+            ref={ref}
+          >
             <div className='flex justify-between shrink-0 p-6 pb-0 mb-8 lg:hidden'>
               <h3 className='text-xl font-bold'>필터</h3>
               <button type='button' onClick={handleClose}>
