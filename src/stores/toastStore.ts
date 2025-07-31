12115628915f84df92d9ac1345d997ee7d43db36
asksa1256@gmail.com <asksa1256@gmail.com>
2025-07-31 17:39:01 +0900

    chore: Toast 타입 주석 설명 추가

--- src/stores/toastStore.ts
+++ src/stores/toastStore.ts
@@ -2,6 +2,14 @@ import { ReactNode } from 'react';
 import { v4 } from 'uuid';
 import { create } from 'zustand';
 
+/**
+ * @typedef {Object} Toast
+ * @property {string} id - 토스트 메시지의 고유 ID
+ * @property {React.ReactNode} message - 토스트에 표시될 메시지 (JSX 또는 문자열 등 ReactNode)
+ * @property {'success' | 'error'} [type] - 토스트의 타입 (성공 또는 에러)
+ * @property {number} [duration] - 토스트가 자동으로 사라지기까지의 시간(ms). 생략 시 자동 제거되지 않음
+ * @property {boolean} [isClosing] - 토스트가 사라지는 중인지 여부 (애니메이션 처리용)
+ */
 interface Toast {
   id: string;
   message: ReactNode;
