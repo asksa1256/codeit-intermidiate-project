b9198d45fd6d74c110fd3d49f6790e009b9bebfa
leesangdal <asksa1256@gmail.com>
2025-07-31 21:34:25 +0900

    feat: slider 데이터 fetch 에러 제어 추가

--- src/components/feature/Slider/SliderSection.tsx
+++ src/components/feature/Slider/SliderSection.tsx
@@ -8,12 +8,18 @@ import { apiClient } from '@/lib/api/apiClient';
 
 const SliderSection = ({ title }: { title?: string | ReactNode }) => {
   const [items, setItems] = useState([]);
+  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
     const fetchItems = async () => {
-      const res = await apiClient.get(`/${TEAM_ID}/wines/recommended?limit=10`);
-      const data = res.data;
-      setItems(data);
+      try {
+        const res = await apiClient.get(`/${TEAM_ID}/wines/recommended?limit=10`);
+        const data = res.data;
+        setItems(data);
+      } catch (err) {
+        console.error(err);
+        setIsError(true);
+      }
     };
     fetchItems();
   }, []);
@@ -22,7 +28,7 @@ const SliderSection = ({ title }: { title?: string | ReactNode }) => {
     <section className='w-full'>
       <article className='bg-gray-100 p-5 md:p-[30px] rounded-xl md:rounded-2xl'>
         {title && <h4 className='text-lg md:text-xl font-bold mb-5 md:mb-[30px]'>{title}</h4>}
-        <ListSlider items={items} />
+        {!isError ? <ListSlider items={items} /> : <p>추천 데이터 가져오기에 실패했습니다.</p>}
       </article>
     </section>
   );
