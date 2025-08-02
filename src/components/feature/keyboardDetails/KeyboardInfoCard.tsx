9fb035a06d96b2e8dc8d9b2f3e788bf61b75a62d
PC2502 <zxcv530@naver.com>
2025-08-02 20:03:37 +0900

    chore: 이미지 임시 인코딩 코드 삭제

--- src/components/feature/keyboardDetails/KeyboardInfoCard.tsx
+++ src/components/feature/keyboardDetails/KeyboardInfoCard.tsx
@@ -11,20 +11,10 @@ interface Props {
 const KeyboardInfoCard = ({ keyboardInfo }: Props) => {
   const { image, name, region, price } = keyboardInfo;
 
-  //이 부분 개발 끝나고 지워줘야 함
-  const imageSlice = image.split('/');
-  const lastImageURL = image.split('/')[imageSlice.length - 1];
-  const encodedImage = encodeURIComponent(lastImageURL);
-  const newImageSrc = image.replace(lastImageURL, encodedImage);
-
   return (
     <section className='flex items-center gap-7 md:gap-15 lg:gap-21 border-1 border-gray-300 rounded-xl w-full md:max-w-285 min-h-53 md:min-h-65 pl-5 pr-7 md:px-15 md:mx-auto md:mb-10 lg:mb-15'>
       <div className='relative shrink-0 w-25 md:w-35 md:self-end'>
-        <KeyboardThumbnail
-          className='md:w-[80%] md:mx-auto'
-          keyboardName={name}
-          imgSrc={newImageSrc}
-        />
+        <KeyboardThumbnail className='md:w-[80%] md:mx-auto' keyboardName={name} imgSrc={image} />
       </div>
       <div className='flex flex-col items-start gap-[10px] md:gap-5 break-keep'>
         <div className='text-xl md:text-2xl lg:text-[30px] font-semibold leading-[100%]'>
