9712065b3b98273e6346a8b81a61a72599ef965a
asksa1256@gmail.com <asksa1256@gmail.com>
2025-08-03 00:29:10 +0900

    fix: user-scalable=0 재설정, 랜딩 페이지 <main> 태그 중복 구조 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,12 +47,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='ko'>
-      <Head>
+      <head>
         <meta
           name='viewport'
           content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'
         />
-      </Head>
+      </head>
       <body className={pretendard.className}>
         <RequireAuth>{children}</RequireAuth>
         <ScrollToTopButton />
