889544912cf636a5ed0d3a3ce06bd6543ec5f838
tasi <taegyung.e@gmail.com>
2025-08-02 22:32:19 +0900

    refactor: CLS 방지 로딩 적용

--- src/components/feature/myProfile/MyprofileSidebar.tsx
+++ src/components/feature/myProfile/MyprofileSidebar.tsx
@@ -6,6 +6,7 @@ import { ChangeEvent, FocusEvent, useRef, useState } from 'react';
 import { useShallow } from 'zustand/shallow';
 
 import CameraIcon from '@/assets/icons/CameraIcon.svg';
+import MyListLoading from '@/components/feature/myProfile/MyListLoading';
 import ButtonDefault from '@/components/ui/ButtonDefault';
 import UserThumbnail from '@/components/ui/UserThumbnail';
 import { DEFAULT_PROFILE_IMG_URL } from '@/constants';
@@ -104,60 +105,64 @@ const MyprofileSidebar = () => {
     setIsSameNickname(isSame);
   };
 
-  if (!user) return null;
-
   return (
     <article className='p-5 border border-gray-300 bg-white rounded-2xl shadow-[0_2px_20px_rgba(0,0,0,0.04)] md:px-10 md:pt-[23px] md:pb-[30px] lg:w-[280px] lg:shrink-0 lg:px-5 lg:py-[39px]'>
-      <div className='flex items-center gap-4 md:items-start md:gap-8 lg:flex-col lg:items-center'>
-        <div className='relative'>
-          <UserThumbnail
-            imgSrc={user.image}
-            userName={user.nickname}
-            className='md:w-20 lg:w-[164px]'
-          />
-          <input
-            type='file'
-            name='imgFileUpload'
-            id='imgFileUpload'
-            accept='image/*'
-            className='hidden'
-            onChange={handleUploadImage}
-            ref={fileRef}
-          />
-          <label
-            htmlFor='imgFileUpload'
-            className='absolute top-[65%] left-[65%] w-[40%] p-1 flex items-center justify-center bg-primary rounded-full cursor-pointer lg:top-0 lg:left-0 lg:w-full lg:h-full lg:bg-primary/30 lg:opacity-0 lg:hover:opacity-100 lg:transition-opacity'
-          >
-            <CameraIcon className='text-white w-[100%] h-[100%] lg:w-10 lg:h-10' />
-          </label>
-          {isUploading && (
-            <div className='absolute top-0 left-0 w-full h-full flex items-center justify-center'>
-              <div className=' w-8 h-8 border-4 mb-4 border-gray-300 border-t-primary rounded-full animate-spin' />
+      {!user ? (
+        <MyListLoading />
+      ) : (
+        <>
+          <div className='flex items-center gap-4 md:items-start md:gap-8 lg:flex-col lg:items-center'>
+            <div className='relative'>
+              <UserThumbnail
+                imgSrc={user.image}
+                userName={user.nickname}
+                className='md:w-20 lg:w-[164px]'
+              />
+              <input
+                type='file'
+                name='imgFileUpload'
+                id='imgFileUpload'
+                accept='image/*'
+                className='hidden'
+                onChange={handleUploadImage}
+                ref={fileRef}
+              />
+              <label
+                htmlFor='imgFileUpload'
+                className='absolute top-[65%] left-[65%] w-[40%] p-1 flex items-center justify-center bg-primary rounded-full cursor-pointer lg:top-0 lg:left-0 lg:w-full lg:h-full lg:bg-primary/30 lg:opacity-0 lg:hover:opacity-100 lg:transition-opacity'
+              >
+                <CameraIcon className='text-white w-[100%] h-[100%] lg:w-10 lg:h-10' />
+              </label>
+              {isUploading && (
+                <div className='absolute top-0 left-0 w-full h-full flex items-center justify-center'>
+                  <div className=' w-8 h-8 border-4 mb-4 border-gray-300 border-t-primary rounded-full animate-spin' />
+                </div>
+              )}
             </div>
-          )}
-        </div>
-        <span className='grow text-xl font-bold md:pt-[7px] md:text-2xl lg:pt-0 lg:min-h-[74px] lg:text-center'>
-          {user.nickname}
-        </span>
-      </div>
-      <div className='mt-5 md:flex md:flex-wrap md:mt-[30px] lg:mt-[48px]'>
-        <span className='block mb-2 text-md font-medium md:w-full md:mb-[10px] md:shrink md:text-base'>
-          닉네임
-        </span>
-        <Input
-          placeholder={user.nickname}
-          className='input h-[42px] py-[9px] mb-1.5 border-gray-300 shadow-none md:grow md:shrink md:basis-0 md:h-[48px] md:py-[11px] md:mb-0 md:mr-6 lg:grow-0 lg:shrink-0 lg:basis-full lg:mr-0 lg:mb-2'
-          ref={nicknameRef}
-          onBlur={checkSameNickname}
-        />
-        <ButtonDefault
-          onClick={handleUpdateNickname}
-          disabled={isSameNickname}
-          className='w-[89px] h-[42px] py-0 px-0 ml-auto text-md font-bold rounded-xl md:w-[116px] md:h-[48px] md:text-base lg:w-[96px] lg:h-[42px]'
-        >
-          변경하기
-        </ButtonDefault>
-      </div>
+            <span className='grow text-xl font-bold md:pt-[7px] md:text-2xl lg:pt-0 lg:min-h-[74px] lg:text-center'>
+              {user.nickname}
+            </span>
+          </div>
+          <div className='mt-5 md:flex md:flex-wrap md:mt-[30px] lg:mt-[48px]'>
+            <span className='block mb-2 text-md font-medium md:w-full md:mb-[10px] md:shrink md:text-base'>
+              닉네임
+            </span>
+            <Input
+              placeholder={user.nickname}
+              className='input h-[42px] py-[9px] mb-1.5 border-gray-300 shadow-none md:grow md:shrink md:basis-0 md:h-[48px] md:py-[11px] md:mb-0 md:mr-6 lg:grow-0 lg:shrink-0 lg:basis-full lg:mr-0 lg:mb-2'
+              ref={nicknameRef}
+              onBlur={checkSameNickname}
+            />
+            <ButtonDefault
+              onClick={handleUpdateNickname}
+              disabled={isSameNickname}
+              className='w-[89px] h-[42px] py-0 px-0 ml-auto text-md font-bold rounded-xl md:w-[116px] md:h-[48px] md:text-base lg:w-[96px] lg:h-[42px]'
+            >
+              변경하기
+            </ButtonDefault>
+          </div>
+        </>
+      )}
     </article>
   );
 };
