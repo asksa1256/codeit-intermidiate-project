5b3c260e0b8f666672a695abc9cb2bae28e5013e
tasi <taegyung.e@gmail.com>
2025-08-03 14:24:29 +0900

    fix: 아이폰에서 hover click 안되는 문제 해결

--- src/components/ui/InfoGuide.tsx
+++ src/components/ui/InfoGuide.tsx
@@ -1,25 +1,57 @@
+'use client';
+
 import Image from 'next/image';
 
-import { ReactNode } from 'react';
+import { ReactNode, useState } from 'react';
+
+import useOutsideClick from '@/hooks/useClickOutside';
+import useWindowWidth from '@/hooks/useWindowWidth';
+import { cn } from '@/utils/style';
 
 interface Props {
   children: ReactNode;
 }
 
 const InfoGuide = ({ children }: Props) => {
+  const innerWidth = useWindowWidth();
+  const [isShow, setIsShow] = useState(false);
+  const isWeb = innerWidth >= 1280;
+  const handleClickHidden = () => {
+    if (isWeb) return;
+    setIsShow(false);
+  };
+  const ref = useOutsideClick(handleClickHidden);
+
+  const handleClickToggle = () => {
+    if (isWeb) return;
+    setIsShow((prev) => !prev);
+  };
+
   return (
-    <div className='relative group'>
-      <Image
-        src='/images/InfoIcon.svg'
-        width={20}
-        height={20}
-        alt='키보드 사진 가이드'
-        className='cursor-pointer'
-      />
-      <div className='hidden absolute top-0 left-[calc(100%+10px)] p-4 border border-gray-300 rounded-2xl bg-white whitespace-nowrap z-10 shadow-[0_4px_20px_0_rgba(0,0,0,0.08)] group-hover:block'>
-        {children}
+    <>
+      {isShow && (
+        <div className={cn('fixed top-0 left-0 right-0 bottom-0', isWeb ? 'hidden' : '')} />
+      )}
+      <div className='relative group' ref={ref}>
+        <Image
+          src='/images/InfoIcon.svg'
+          width={20}
+          height={20}
+          alt='키보드 사진 가이드'
+          className='cursor-pointer'
+          onClick={handleClickToggle}
+        />
+        <div
+          className={cn(
+            'hidden absolute top-0 left-[calc(100%+10px)] p-4 border border-gray-300 rounded-2xl bg-white whitespace-nowrap z-10 shadow-[0_4px_20px_0_rgba(0,0,0,0.08)]',
+            isWeb ? 'group-hover:block' : '',
+            isShow ? 'block' : '',
+          )}
+        >
+          {children}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
