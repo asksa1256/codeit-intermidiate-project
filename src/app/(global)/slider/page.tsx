547e9dfa06d72e13b4b6aaadcf7000caf447196a
asksa1256 <asksa1256@gmail.com>
2026-01-26 13:46:25 +0900

    chore: slider 테스트 페이지 제거

--- src/app/(global)/slider/page.tsx
+++
@@ -1,7 +0,0 @@
-import SliderSection from '@/components/feature/Slider/SliderSection';
-
-const SliderPage = () => {
-  return <SliderSection title='이번 달 추천 키보드' />;
-};
-
-export default SliderPage;
